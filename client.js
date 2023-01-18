b6b89a3511e802697f35971bb9988f3945d5d959
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-18 12:49:30 -0500

    created client file and imported it in play.js file

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Connected to server."); // code that does something when the connection is first established
+  });
+
+  conn.on("data", (data) => {
+    console.log("Server says:", data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
