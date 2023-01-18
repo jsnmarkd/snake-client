e17a04facc1effedcb554b06f1fa9a52b93b08bf
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-18 15:59:10 -0500

    Implemented setIntervals so that it moves like the actual game

--- input.js
+++ input.js
@@ -9,23 +9,38 @@ const setupInput = function (conn) {
   return stdin;
 };
 
+let interval;
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key !== 'f') {
+    clearInterval(interval);
+  }
   if (key === 'w') {
+    interval = setInterval(() => {
       connection.write("Move: up");
+    }, 70);
   }
   if (key === 'a') {
+    interval = setInterval(() => {
       connection.write("Move: left");
+    }, 50);
+      // connection.write("Move: left");
   }
   if (key === 's') {
+    interval = setInterval(() => {
       connection.write("Move: down");
+    }, 70);
+      // connection.write("Move: down");
   }
   if (key === 'd') {
+    interval = setInterval(() => {
       connection.write("Move: right");
+    }, 50);
+      // connection.write("Move: right");
     }
-  if (key === 'f') {  
+  if (key === 'f') {
     connection.write("Say: GG");
   }
 };
