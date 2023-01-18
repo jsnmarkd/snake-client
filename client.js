fa67a5ddbb78b747a692262ef918927cb3955a72
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-18 15:17:48 -0500

    Movement commands are being sent to the server

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost", // IP address here,
+    host: "165.227.47.243", // IP address here,
     port: 50541 // PORT number here,
   });
 
